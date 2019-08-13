cd435bad791c6cfccdd1c1c94e5d43ab64dacf1d
aknochaco <54080904+aknochaco@users.noreply.github.com>
2019-08-13 21:52:16 +0200

    Add files via upload
    Added upgrades

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
 var gameData = {
   pixels: 0,
-  pixelsPerClick: 1
+  pixelsPerClick: 1,
+  pixelsPerClickCost: 10
 }
 
 function renderpixels() {
   gameData.pixels += gameData.pixelsPerClick
   document.getElementById('pixelsrendered').innerHTML = gameData.pixels + " Pixels Rendered"
+}
+
+function buypixelsPerClick() {
+  if (gameData.pixels >= gameData.pixelsPerClickCost) {
+    gameData.pixels -= gameData.pixelsPerClickCost
+    gameData.pixelsPerClick += 1
+    gameData.pixelsPerClickCost *= 2
+    document.getElementById("pixelsrendered").innerHTML = gameData.pixels + " Pixels Rendered"
+    document.getElementById("perClickUpgrade").innerHTML = "Upgrade Computer (Level " + gameData.pixelsPerClick + ") Cost : " + gameData.pixelsPerClickCost + " Pixels"
+  }
 }
