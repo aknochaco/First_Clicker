eab071673da081390ba03465cdb7e92346a97635
aknochaco <54080904+aknochaco@users.noreply.github.com>
2019-08-14 10:55:23 +0200

    Add files via upload
    progress bar added

--- main.js
+++ main.js
@@ -19,6 +19,23 @@ function buypixelsPerClick() {
   }
 }
 
+function move() {
+  var elem = document.getElementById("myBar"); 
+  var width = 0;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 100) {
+      clearInterval(id);
+    } else {
+      width ++; 
+      elem.style.width = width + '%'; 
+    }
+  }
+}
+
 var mainGameLoop = window.setInterval(function() {
  renderpixels()
+ move()
 }, 1000)
+
+
